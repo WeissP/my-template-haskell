de46789b6e5a792b6b072bbb7bbfdea204399496
weiss <kingbaiing@163.com>
2024-03-08 15:08:22 +0100

    personal version

--- flake.nix
+++ flake.nix
@@ -1,10 +1,13 @@
 {
   description = "PKGNAME";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+  };
 
-  outputs = inputs:
+  outputs = inputs@{ self, flake-utils, devshell, nixpkgs, ... }:
     let
       overlay = final: prev: {
         haskell = prev.haskell // {
@@ -13,33 +16,48 @@
               PKGNAME = hfinal.callCabal2nix "PKGNAME" ./. { };
             };
         };
-        PKGNAME = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.PKGNAME;
+        PKGNAME = final.haskellPackages.PKGNAME;
       };
       perSystem = system:
         let
-          pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
-          hspkgs = pkgs.haskellPackages;
-        in
-        {
-          devShells = rec {
-            default = PKGNAME-shell;
-            PKGNAME-shell = hspkgs.shellFor {
-              withHoogle = true;
-              packages = p: [ p.PKGNAME ];
-              buildInputs = [
-                hspkgs.cabal-install
-                hspkgs.haskell-language-server
-                hspkgs.hlint
-                hspkgs.ormolu
-                pkgs.bashInteractive
-              ];
-            };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ overlay devshell.overlays.default ];
+          };
+          ghcVersion = "ghc948";
+          hspkgs = pkgs.haskell.packages.${ghcVersion};
+          devShells.shellFor = hspkgs.shellFor {
+            packages = p: [ p.PKGNAME ];
+            withHoogle = true;
+          };
+        in {
+          devShells.default = pkgs.devshell.mkShell {
+            name = "PKGNAME";
+            imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+            packagesFrom = [ devShells.shellFor ];
+            commands = [
+              (let cabal = pkgs.cabal-install;
+              in {
+                name = cabal.pname;
+                help = cabal.meta.description;
+                package = cabal;
+                category = "tools";
+              })
+              (let hls = hspkgs.haskell-language-server;
+              in {
+                name = hls.pname;
+                help = hls.meta.description;
+                package = hls;
+                category = "tools";
+              })
+            ];
+            packages = [ hspkgs.hlint hspkgs.fourmolu hspkgs.cabal-fmt ];
           };
           packages = rec {
             default = PKGNAME;
             PKGNAME = pkgs.PKGNAME;
           };
         };
-    in
-    { inherit overlay; } // inputs.flake-utils.lib.eachDefaultSystem perSystem;
+    in { inherit overlay; } // flake-utils.lib.eachDefaultSystem perSystem;
+
 }
