ea9dc6c6b8ef596f63a836d681dbf0f07af1df17
weiss <kingbaiing@163.com>
2024-04-16 14:28:20 +0200

    use haskellNix

--- flake.nix
+++ flake.nix
@@ -2,62 +2,59 @@
   description = "PKGNAME";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     devshell.url = "github:numtide/devshell";
   };
 
-  outputs = inputs@{ self, flake-utils, devshell, nixpkgs, ... }:
-    let
-      overlay = final: prev: {
-        haskell = prev.haskell // {
-          packageOverrides = hfinal: hprev:
-            prev.haskell.packageOverrides hfinal hprev // {
-              PKGNAME = hfinal.callCabal2nix "PKGNAME" ./. { };
+  nixConfig = {
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys =
+      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+  };
+
+  outputs = inputs@{ self, flake-utils, haskellNix, devshell, nixpkgs, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            # This overlay adds our project to pkgs
+            PKGNAME = final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc982";
             };
+          })
+          devshell.overlays.default
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
         };
-        PKGNAME = final.haskellPackages.PKGNAME;
-      };
-      perSystem = system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ overlay devshell.overlays.default ];
-          };
-          ghcVersion = "ghc948";
-          hspkgs = pkgs.haskell.packages.${ghcVersion};
-          devShells.shellFor = hspkgs.shellFor {
-            packages = p: [ p.PKGNAME ];
+        flake = pkgs.PKGNAME.flake { };
+        shellWithToml = tomls:
+          pkgs.PKGNAME.shellFor {
+            exactDeps = false;
             withHoogle = true;
-          };
-        in {
-          devShells.default = pkgs.devshell.mkShell {
-            name = "PKGNAME";
-            imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
-            packagesFrom = [ devShells.shellFor ];
-            commands = [
-              (let cabal = pkgs.cabal-install;
-              in {
-                name = cabal.pname;
-                help = cabal.meta.description;
-                package = cabal;
-                category = "tools";
-              })
-              (let hls = hspkgs.haskell-language-server;
-              in {
-                name = hls.pname;
-                help = hls.meta.description;
-                package = hls;
-                category = "tools";
+            inputsFrom = [
+              (pkgs.devshell.mkShell {
+                name = "PKGNAME";
+                imports = map pkgs.devshell.importTOML
+                  ([ ./env_config/common.toml ] ++ tomls);
               })
             ];
-            packages = [ hspkgs.hlint hspkgs.fourmolu hspkgs.cabal-fmt ];
+            tools = {
+              cabal = "latest";
+              fourmolu = "latest";
+              hlint = "latest";
+              cabal-fmt = "latest";
+              haskell-language-server = "latest";
+            };
           };
-          packages = rec {
-            default = PKGNAME;
-            PKGNAME = pkgs.PKGNAME;
-          };
-        };
-    in { inherit overlay; } // flake-utils.lib.eachDefaultSystem perSystem;
+      in flake // {
+        packages = { default = flake.packages."PKGNAME:exe:PKGNAME-exe"; };
+        devShells = { default = shellWithToml [ ]; };
+      });
 
 }
